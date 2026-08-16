8ce1109e422d711ba30a892fa19309018e8f52b8
tek-sys-hub <tekawasthi87@gmail.com>
2026-08-16 13:51:02 +0545

    Initial Commit

---
+++ src/main/java/com/cherry/stdmngt/StdmngtApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.stdmngt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdmngtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdmngtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherry/stdmngt/StdmngtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.stdmngt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdmngtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
